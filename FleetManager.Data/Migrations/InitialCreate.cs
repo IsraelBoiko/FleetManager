ea2a154f3b672cbd8a5ec05026b9a68f9dc12833
Israel José Boiko <israelboiko@hotmail.com>
2019-07-15 09:21:16 -0300

    Utilização do Migrate para iniciar o banco de dados.

---
+++ FleetManager.Data/Migrations/InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FleetManager.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vehicles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Chassi = table.Column<string>(maxLength: 100, nullable: false),
+                    Type = table.Column<int>(nullable: false),
+                    Color = table.Column<string>(maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("VehiclePk", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "VehicleNk",
+                table: "Vehicles",
+                column: "Chassi",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vehicles");
+        }
+    }
+}
