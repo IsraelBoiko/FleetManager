5ff179384bfaf3b89d299c26ee5b1394a147d6d4
Israel José Boiko <israelboiko@hotmail.com>
2019-07-15 07:57:33 -0300

    Repositório de veículos.

---
+++ FleetManager.Data/Concrete/VehicleRepository.cs
@@ -0,0 +1,29 @@
+﻿using FleetManager.Data.Entity;
+using FleetManager.Model;
+using System.Linq;
+
+namespace FleetManager.Data.Concrete
+{
+    public class VehicleRepository : IVehicleRepository
+    {
+        public VehicleRepository(FleetContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public FleetContext DbContext { get; }
+
+        public void Add(Vehicle model)
+        {
+            DbContext.Vehicles.Add(model);
+
+            DbContext.SaveChanges();
+        }
+
+        public bool ChassiExists(string chassi) => DbContext.Vehicles
+            .Any(v => v.Chassi == chassi);
+
+        public Vehicle ChassiFind(string chassi) => DbContext.Vehicles
+            .SingleOrDefault(v => v.Chassi == chassi);
+    }
+}
