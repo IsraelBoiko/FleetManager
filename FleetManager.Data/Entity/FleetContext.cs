d2f1e7c069f731f0d57f5984eadf0986915945d0
Israel José Boiko <israelboiko@hotmail.com>
2019-07-15 07:57:17 -0300

    Banco de dados de veículos.

---
+++ FleetManager.Data/Entity/FleetContext.cs
@@ -0,0 +1,15 @@
+﻿using FleetManager.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace FleetManager.Data.Entity
+{
+    public class FleetContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data source=fleet.db");
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+    }
+}
