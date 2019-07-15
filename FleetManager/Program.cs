03fa527b3c430a76feb83bea1911644ad0cf094b
Israel José Boiko <israelboiko@hotmail.com>
2019-07-15 09:21:37 -0300

    Testando a migração do banco de dados.

--- FleetManager/Program.cs
+++ FleetManager/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Linq;
 
 namespace FleetManager
 {
@@ -6,7 +10,35 @@ namespace FleetManager
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new Program().Start(new ServiceCollection());
+        }
+
+        public void Start(IServiceCollection services)
+        {
+            ConfigureServices(services);
+
+            var provider = services.BuildServiceProvider();
+
+            using (var scope = provider.CreateScope())
+            using (var dbContext = scope.ServiceProvider.GetService<Data.Entity.FleetContext>())
+            {
+                dbContext.Database.Migrate();
+
+                if (!dbContext.Vehicles.Any(e => e.Chassi == "ABC"))
+                {
+                    dbContext.Vehicles.Add(new Model.Vehicle("ABC", Model.VehicleType.Bus, "Azul"));
+                    dbContext.SaveChanges();
+                }
+
+                var all = dbContext.Vehicles.ToList();
+            }
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var baseDirectory = AppContext.BaseDirectory;
+
+            services.AddDbContext<Data.Entity.FleetContext>(options => options.UseSqlite($"Data source={Path.Combine(baseDirectory, "fleet.db")}"));
         }
     }
 }
